20c03ae5308e4e50419d851cbf4b6eb9ccee949e
Aaron Gilbert <marth8880@gmail.com>
2021-09-11 16:42:10 +0100

    New config checking

--- ME5_ConfigCheck.lua
+++ ME5_ConfigCheck.lua
@@ -22,8 +22,10 @@
 
 print("ME5_ConfigCheck: Entered")
 
--- The base path for all of the config files.
-local path = "..\\..\\addon\\ME5\\data\\_LVL_PC\\cfg\\"
+ReadDataFile("..\\..\\addon\\ME5\\data\\_LVL_PC\\ConfigUtility\\meumodconfig.script")
+ScriptCB_DoFile("meumodconfig")
+
+local settings = gMEUModConfig
 
 ---
 -- Checks the given setting and its value based on /setting/ and /value/.
@@ -31,19 +33,15 @@ local path = "..\\..\\addon\\ME5\\data\\_LVL_PC\\cfg\\"
 -- @param #int value		The value of the setting to check for.
 -- @return 					1 if setting's value is valid, 0 if invalid. Will also return 0 if one of the function parameters wasn't inputted.
 -- 
-local function CheckSetting(setting, value)
+local function CheckSetting(key)
 	
 	-- Make sure all parameters have been inputted
-	if not setting then
-		print("ME5_ConfigCheck.CheckSetting(): WARNING: Argument #0 'setting' not set! Exiting function")
-		return 0
-	end
-	if not value then
-		print("ME5_ConfigCheck.CheckSetting(): WARNING: Argument #1 'value' not set! Exiting function")
+	if not key then
+		print("ME5_ConfigCheck.CheckSetting(): WARNING: Argument #0 'key' not set! Exiting function")
 		return 0
 	end
 	
-	return ScriptCB_IsFileExist("..\\..\\addon\\ME5\\data\\_LVL_PC\\cfg\\"..setting.."_"..value)
+	return settings[key]
 end
 
 
@@ -52,14 +50,15 @@ print("ME5_ConfigCheck: Checking configuration parameters...")
 -- ======================
 --  AI HEROES
 -- ======================
-if CheckSetting("cfg_AIHeroes", 0) == 1 then
+if CheckSetting("cfg_AIHeroes") == 0 then
 	print("ME5_ConfigCheck: AI Heroes are DISABLED")
 	ME5_AIHeroes = 0
 	
-elseif CheckSetting("cfg_AIHeroes", 1) == 1 then
+elseif CheckSetting("cfg_AIHeroes") == 1 then
 	print("ME5_ConfigCheck: AI Heroes are ENABLED")
 	ME5_AIHeroes = 1
 	
+
 else
 	print("ME5_ConfigCheck: Error! ME5_AIHeroes flag not found or invalid! Defaulting to ENABLED")
 	ME5_AIHeroes = 1
@@ -69,15 +68,15 @@ end
 -- ======================
 --  MENU INTERFACE STYLE
 -- ======================
-if CheckSetting("cfg_CustomGUIEnabled", 0) == 1 then
+if CheckSetting("cfg_CustomGUIEnabled") == 0 then
 	print("ME5_ConfigCheck: Menu Interface Style is STAR WARS BATTLEFRONT II")
 	ME5_CustomGUIEnabled = 0
 	
-elseif CheckSetting("cfg_CustomGUIEnabled", 1) == 1 then
+elseif CheckSetting("cfg_CustomGUIEnabled") == 1 then
 	print("ME5_ConfigCheck: Menu Interface Style is MASS EFFECT: UNIFICATION")
 	ME5_CustomGUIEnabled = 1
 	
-elseif CheckSetting("cfg_CustomGUIEnabled", 2) == 1 then
+elseif CheckSetting("cfg_CustomGUIEnabled") == 2 then
 	print("ME5_ConfigCheck: Menu Interface Style is MASS EFFECT 3")
 	ME5_CustomGUIEnabled = 2
 	
@@ -90,11 +89,11 @@ end
 -- ======================
 --  CUSTOM HUD
 -- ======================
-if CheckSetting("cfg_CustomHUD", 0) == 1 then
+if CheckSetting("cfg_CustomHUD") == 0 then
 	print("ME5_ConfigCheck: Custom HUD is DISABLED")
 	ME5_CustomHUD = 0
 	
-elseif CheckSetting("cfg_CustomHUD", 1) == 1 then
+elseif CheckSetting("cfg_CustomHUD") == 1 then
 	print("ME5_ConfigCheck: Custom HUD is ENABLED")
 	ME5_CustomHUD = 1
 	rema_noHUD = true
@@ -107,23 +106,23 @@ end
 -- ======================
 --  GAMEPLAY DIFFICULTY
 -- ======================
-if CheckSetting("cfg_Difficulty", 0) == 1 then
+if CheckSetting("cfg_Difficulty") == 0 then
 	print("ME5_ConfigCheck: Difficulty is CASUAL")
 	ME5_Difficulty = 1
 	
-elseif CheckSetting("cfg_Difficulty", 1) == 1 then
+elseif CheckSetting("cfg_Difficulty") == 1 then
 	print("ME5_ConfigCheck: Difficulty is NORMAL")
 	ME5_Difficulty = 2
 	
-elseif CheckSetting("cfg_Difficulty", 2) == 1 then
+elseif CheckSetting("cfg_Difficulty") == 2 then
 	print("ME5_ConfigCheck: Difficulty is VETERAN")
 	ME5_Difficulty = 3
 	
-elseif CheckSetting("cfg_Difficulty", 3) == 1 then
+elseif CheckSetting("cfg_Difficulty") == 3 then
 	print("ME5_ConfigCheck: Difficulty is HARDCORE")
 	ME5_Difficulty = 4
 	
-elseif CheckSetting("cfg_Difficulty", 4) == 1 then
+elseif CheckSetting("cfg_Difficulty") == 4 then
 	print("ME5_ConfigCheck: Difficulty is INSANITY")
 	ME5_Difficulty = 5
 	
@@ -136,11 +135,11 @@ end
 -- ======================
 --  CARNAGE MODE
 -- ======================
-if CheckSetting("cfg_CarnageMode", 0) == 1 then
+if CheckSetting("cfg_CarnageMode") == 0 then
 	print("ME5_ConfigCheck: Carnage Mode is DISABLED")
 	ME5_CarnageMode = 0
 	
-elseif CheckSetting("cfg_CarnageMode", 1) == 1 then
+elseif CheckSetting("cfg_CarnageMode") == 1 then
 	print("ME5_ConfigCheck: Carnage Mode is ENABLED")
 	ME5_CarnageMode = 1
 	
@@ -153,11 +152,11 @@ end
 -- ======================
 --  HEALTH FUNCTIONALITY
 -- ======================
-if CheckSetting("cfg_HealthFunc", 0) == 1 then
+if CheckSetting("cfg_HealthFunc") == 0 then
 	print("ME5_ConfigCheck: Health Functionality is AUTO-REGEN")
 	ME5_HealthFunc = 1
 	
-elseif CheckSetting("cfg_HealthFunc", 1) == 1 then
+elseif CheckSetting("cfg_HealthFunc") == 1 then
 	print("ME5_ConfigCheck: Health Functionality is PICKUPS")
 	ME5_HealthFunc = 2
 	
@@ -170,11 +169,11 @@ end
 -- ======================
 --  SHIELD FUNCTIONALITY
 -- ======================
-if CheckSetting("cfg_ShieldFunc", 0) == 1 then
+if CheckSetting("cfg_ShieldFunc") == 0 then
 	print("ME5_ConfigCheck: Shield Functionality is REGEN")
 	ME5_ShieldFunc = 1
 	
-elseif CheckSetting("cfg_ShieldFunc", 1) == 1 then
+elseif CheckSetting("cfg_ShieldFunc") == 1 then
 	print("ME5_ConfigCheck: Shield Functionality is PICKUPS")
 	ME5_ShieldFunc = 2
 	
@@ -187,11 +186,11 @@ end
 -- ======================
 --  SHIELD REGEN FUNCTIONALITY
 -- ======================
-if CheckSetting("cfg_ShieldRegen", 0) == 1 then
+if CheckSetting("cfg_ShieldRegen") == 0 then
 	print("ME5_ConfigCheck: Shield Regen Functionality (Player Only) is AUTOMATIC")
 	ME5_ShieldRegen = 0
 	
-elseif CheckSetting("cfg_ShieldRegen", 1) == 1 then
+elseif CheckSetting("cfg_ShieldRegen") == 1 then
 	print("ME5_ConfigCheck: Shield Regen Functionality (Player Only) is DEFERRED")
 	ME5_ShieldRegen = 1
 	
@@ -204,11 +203,11 @@ end
 -- ======================
 --  COLLECTOR HERO CLASS
 -- ======================
-if CheckSetting("cfg_HeroClassCOL", 0) == 1 then
+if CheckSetting("cfg_HeroClassCOL") == 0 then
 	print("ME5_ConfigCheck: COL Hero Class is RANDOM")
 	ME5_HeroClassCOL = math.random(1, MAX_COL_HERO_COUNT)
 	
-elseif CheckSetting("cfg_HeroClassCOL", 1) == 1 then
+elseif CheckSetting("cfg_HeroClassCOL") == 1 then
 	print("ME5_ConfigCheck: COL Hero Class is HARBINGER")
 	ME5_HeroClassCOL = 1
 	
@@ -221,15 +220,15 @@ end
 -- ======================
 --  EVOLVED GETH HERO CLASS
 -- ======================
-if CheckSetting("cfg_HeroClassEVG", 0) == 1 then
+if CheckSetting("cfg_HeroClassEVG") == 0 then
 	print("ME5_ConfigCheck: EVG Hero Class is RANDOM")
 	ME5_HeroClassEVG = math.random(1, MAX_EVG_HERO_COUNT)
 	
-elseif CheckSetting("cfg_HeroClassEVG", 1) == 1 then
+elseif CheckSetting("cfg_HeroClassEVG") == 1 then
 	print("ME5_ConfigCheck: EVG Hero Class is GETH PRIME (ME2)")
 	ME5_HeroClassEVG = 1
 	
-elseif CheckSetting("cfg_HeroClassEVG", 2) == 1 then
+elseif CheckSetting("cfg_HeroClassEVG") == 2 then
 	print("ME5_ConfigCheck: EVG Hero Class is GETH PRIME (ME3)")
 	ME5_HeroClassEVG = 2
 	
@@ -242,15 +241,15 @@ end
 -- ======================
 --  HERETIC GETH HERO CLASS
 -- ======================
-if CheckSetting("cfg_HeroClassGTH", 0) == 1 then
+if CheckSetting("cfg_HeroClassGTH") == 0 then
 	print("ME5_ConfigCheck: GTH Hero Class is RANDOM")
 	ME5_HeroClassGTH = math.random(1, MAX_GTH_HERO_COUNT)
 	
-elseif CheckSetting("cfg_HeroClassGTH", 1) == 1 then
+elseif CheckSetting("cfg_HeroClassGTH") == 1 then
 	print("ME5_ConfigCheck: GTH Hero Class is GETH PRIME (ME2)")
 	ME5_HeroClassGTH = 1
 	
-elseif CheckSetting("cfg_HeroClassGTH", 2) == 1 then
+elseif CheckSetting("cfg_HeroClassGTH") == 2 then
 	print("ME5_ConfigCheck: GTH Hero Class is GETH PRIME (ME3)")
 	ME5_HeroClassGTH = 2
 	
@@ -263,15 +262,15 @@ end
 -- ======================
 --  ALLIANCE HERO CLASS
 -- ======================
-if CheckSetting("cfg_HeroClassSSV", 0) == 1 then
+if CheckSetting("cfg_HeroClassSSV") == 0 then
 	print("ME5_ConfigCheck: SSV Hero Class is RANDOM")
 	ME5_HeroClassSSV = math.random(1, MAX_SSV_HERO_COUNT)
 	
-elseif CheckSetting("cfg_HeroClassSSV", 1) == 1 then
+elseif CheckSetting("cfg_HeroClassSSV") == 1 then
 	print("ME5_ConfigCheck: SSV Hero Class is SHEPARD")
 	ME5_HeroClassSSV = 1
 	
-elseif CheckSetting("cfg_HeroClassSSV", 2) == 1 then
+elseif CheckSetting("cfg_HeroClassSSV") == 2 then
 	print("ME5_ConfigCheck: SSV Hero Class is JACK")
 	ME5_HeroClassSSV = 2
 	
@@ -284,31 +283,31 @@ end
 -- ======================
 --  SHEPARD CLASS
 -- ======================
-if CheckSetting("cfg_ShepardClass", 0) == 1 then
+if CheckSetting("cfg_ShepardClass") == 0 then
 	print("ME5_ConfigCheck: Shepard Class is RANDOM")
 	ME5_ShepardClass = math.random(1, MAX_SHEP_CLASS_COUNT)
 	
-elseif CheckSetting("cfg_ShepardClass", 1) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 1 then
 	print("ME5_ConfigCheck: Shepard Class is SOLDIER")
 	ME5_ShepardClass = 1
 	
-elseif CheckSetting("cfg_ShepardClass", 2) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 2 then
 	print("ME5_ConfigCheck: Shepard Class is INFILTRATOR")
 	ME5_ShepardClass = 2
 	
-elseif CheckSetting("cfg_ShepardClass", 3) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 3 then
 	print("ME5_ConfigCheck: Shepard Class is ENGINEER")
 	ME5_ShepardClass = 3
 	
-elseif CheckSetting("cfg_ShepardClass", 4) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 4 then
 	print("ME5_ConfigCheck: Shepard Class is ADEPT")
 	ME5_ShepardClass = 4
 	
-elseif CheckSetting("cfg_ShepardClass", 5) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 5 then
 	print("ME5_ConfigCheck: Shepard Class is SENTINEL")
 	ME5_ShepardClass = 5
 	
-elseif CheckSetting("cfg_ShepardClass", 6) == 1 then
+elseif CheckSetting("cfg_ShepardClass") == 6 then
 	print("ME5_ConfigCheck: Shepard Class is VANGUARD")
 	ME5_ShepardClass = 6
 	
@@ -321,15 +320,15 @@ end
 -- ======================
 --  SHEPARD GENDER
 -- ======================
-if CheckSetting("cfg_ShepardGender", 0) == 1 then
+if CheckSetting("cfg_ShepardGender") == 0 then
 	print("ME5_ConfigCheck: Shepard Gender is RANDOM")
 	ME5_ShepardGender = math.random(1, MAX_SHEP_GENDER_COUNT)
 	
-elseif CheckSetting("cfg_ShepardGender", 1) == 1 then
+elseif CheckSetting("cfg_ShepardGender") == 1 then
 	print("ME5_ConfigCheck: Shepard Gender is MALE")
 	ME5_ShepardGender = 1
 	
-elseif CheckSetting("cfg_ShepardGender", 2) == 1 then
+elseif CheckSetting("cfg_ShepardGender") == 2 then
 	print("ME5_ConfigCheck: Shepard Gender is FEMALE")
 	ME5_ShepardGender = 2
 	
@@ -342,27 +341,27 @@ end
 -- ======================
 --  SIDE VARIATION
 -- ======================
-if CheckSetting("cfg_SideVar", 0) == 1 then
+if CheckSetting("cfg_SideVar") == 0 then
 	print("ME5_ConfigCheck: Side Variation is RANDOM")
 	ME5_SideVar = math.random(1, MAX_FACTION_COUNT)
 	
-elseif CheckSetting("cfg_SideVar", 1) == 1 then
+elseif CheckSetting("cfg_SideVar") == 1 then
 	print("ME5_ConfigCheck: Side Variation is SSVxGTH")
 	ME5_SideVar = 1
 	
-elseif CheckSetting("cfg_SideVar", 2) == 1 then
+elseif CheckSetting("cfg_SideVar") == 2 then
 	print("ME5_ConfigCheck: Side Variation is SSVxCOL")
 	ME5_SideVar = 2
 	
-elseif CheckSetting("cfg_SideVar", 3) == 1 then
+elseif CheckSetting("cfg_SideVar") == 3 then
 	print("ME5_ConfigCheck: Side Variation is EVGxGTH")
 	ME5_SideVar = 3
 	
-elseif CheckSetting("cfg_SideVar", 4) == 1 then
+elseif CheckSetting("cfg_SideVar") == 4 then
 	print("ME5_ConfigCheck: Side Variation is EVGxCOL")
 	ME5_SideVar = 4
 	
-elseif CheckSetting("cfg_SideVar", 5) == 1 then
+elseif CheckSetting("cfg_SideVar") == 5 then
 	print("ME5_ConfigCheck: Side Variation is SSVxRPR")
 	ME5_SideVar = 5
 	
@@ -376,19 +375,19 @@ end
 --  EDEN PRIME WEATHER
 -- ======================
 if not ScriptCB_InMultiplayer() then
-	if CheckSetting("cfg_MapVarEDN", 0) == 1 then
+	if CheckSetting("cfg_MapVarEDN") == 0 then
 		print("ME5_ConfigCheck: Eden Prime Weather is RANDOM")
 		ME5_MapVarEDN = math.random(1,3)
 		
-	elseif CheckSetting("cfg_MapVarEDN", 1) == 1 then
+	elseif CheckSetting("cfg_MapVarEDN") == 1 then
 		print("ME5_ConfigCheck: Eden Prime Weather is STORMY")
 		ME5_MapVarEDN = 1
 		
-	elseif CheckSetting("cfg_MapVarEDN", 2) == 1 then
+	elseif CheckSetting("cfg_MapVarEDN") == 2 then
 		print("ME5_ConfigCheck: Eden Prime Weather is CLOUDY")
 		ME5_MapVarEDN = 2
 		
-	elseif CheckSetting("cfg_MapVarEDN", 3) == 1 then
+	elseif CheckSetting("cfg_MapVarEDN") == 3 then
 		print("ME5_ConfigCheck: Eden Prime Weather is NIGHT")
 		ME5_MapVarEDN = 3
 		
@@ -406,15 +405,15 @@ end
 --  VIRMIRE PLAY AREA
 -- ======================
 if not ScriptCB_InMultiplayer() then
-	if CheckSetting("cfg_MapVarVRM", 0) == 1 then
+	if CheckSetting("cfg_MapVarVRM") == 0 then
 		print("ME5_ConfigCheck: Virmire Play Area is RANDOM")
 		ME5_MapVarVRM = math.random(1,2)
 		
-	elseif CheckSetting("cfg_MapVarVRM", 1) == 1 then
+	elseif CheckSetting("cfg_MapVarVRM") == 1 then
 		print("ME5_ConfigCheck: Virmire Play Area is UPPER LEVEL")
 		ME5_MapVarVRM = 1
 		
-	elseif CheckSetting("cfg_MapVarVRM", 2) == 1 then
+	elseif CheckSetting("cfg_MapVarVRM") == 2 then
 		print("ME5_ConfigCheck: Virmire Play Area is LOWER LEVEL")
 		ME5_MapVarVRM = 2
 		
@@ -431,11 +430,11 @@ end
 -- ======================
 --  SOL MAP MUSIC
 -- ======================
-if CheckSetting("cfg_SolMapMusic", 0) == 1 then
+if CheckSetting("cfg_SolMapMusic") == 0 then
 	print("ME5_ConfigCheck: 'Sol Map Pack' Map Music is AMBIENT SOUNDSCAPE")
 	ME5_SolMapMusic = 0
 	
-elseif CheckSetting("cfg_SolMapMusic", 1) == 1 then
+elseif CheckSetting("cfg_SolMapMusic") == 1 then
 	print("ME5_ConfigCheck: 'Sol Map Pack' Map Music is MASS EFFECT")
 	ME5_SolMapMusic = 1
 	
@@ -448,15 +447,15 @@ end
 -- ======================
 --  FACTION ANNOUNCER VO
 -- ======================
-if CheckSetting("cfg_FactionVO", 0) == 1 then
+if CheckSetting("cfg_FactionVO") == 0 then
 	print("ME5_ConfigCheck: Faction Announcer VO is DISABLED")
 	ME5_FactionVO = 0
 	
-elseif CheckSetting("cfg_FactionVO", 1) == 1 then
+elseif CheckSetting("cfg_FactionVO") == 1 then
 	print("ME5_ConfigCheck: Faction Announcer VO is QUIET")
 	ME5_FactionVO = 1
 	
-elseif CheckSetting("cfg_FactionVO", 2) == 1 then
+elseif CheckSetting("cfg_FactionVO") == 2 then
 	print("ME5_ConfigCheck: Faction Announcer VO is LOUD")
 	ME5_FactionVO = 2
 	
@@ -469,11 +468,11 @@ end
 -- ======================
 --  LOW HEALTH SOUND
 -- ======================
-if CheckSetting("cfg_LowHealthSound", 0) == 1 then
+if CheckSetting("cfg_LowHealthSound") == 0 then
 	print("ME5_ConfigCheck: 'Low Health' Feedback Sounds are DISABLED")
 	ME5_LowHealthSound = 0
 	
-elseif CheckSetting("cfg_LowHealthSound", 1) == 1 then
+elseif CheckSetting("cfg_LowHealthSound") == 1 then
 	print("ME5_ConfigCheck: 'Low Health' Feedback Sounds are ENABLED")
 	ME5_LowHealthSound = 1
 	
@@ -486,15 +485,15 @@ end
 -- ======================
 --  KILL SOUND
 -- ======================
-if CheckSetting("cfg_KillSound", 0) == 1 then
+if CheckSetting("cfg_KillSound") == 0 then
 	print("ME5_ConfigCheck: 'Enemy Killed' Feedback Sounds are DISABLED")
 	ME5_KillSound = 0
 	
-elseif CheckSetting("cfg_KillSound", 1) == 1 then
+elseif CheckSetting("cfg_KillSound") == 1 then
 	print("ME5_ConfigCheck: 'Enemy Killed' Feedback Sounds are QUIET")
 	ME5_KillSound = 1
 	
-elseif CheckSetting("cfg_KillSound", 2) == 1 then
+elseif CheckSetting("cfg_KillSound") == 2 then
 	print("ME5_ConfigCheck: 'Enemy Killed' Feedback Sounds are LOUD")
 	ME5_KillSound = 2
 	
@@ -507,15 +506,15 @@ end
 -- ======================
 --  PLAYER DAMAGE SOUNDS
 -- ======================
-if CheckSetting("cfg_PlayerDmgSound", 0) == 1 then
+if CheckSetting("cfg_PlayerDmgSound") == 0 then
 	print("ME5_ConfigCheck: 'Player Damaged' Feedback Sounds are DISABLED")
 	ME5_PlayerDmgSound = 0
 	
-elseif CheckSetting("cfg_PlayerDmgSound", 1) == 1 then
+elseif CheckSetting("cfg_PlayerDmgSound") == 1 then
 	print("ME5_ConfigCheck: 'Player Damaged' Feedback Sounds are QUIET")
 	ME5_PlayerDmgSound = 1
 	
-elseif CheckSetting("cfg_PlayerDmgSound", 2) == 1 then
+elseif CheckSetting("cfg_PlayerDmgSound") == 2 then
 	print("ME5_ConfigCheck: 'Player Damaged' Feedback Sounds are LOUD")
 	ME5_PlayerDmgSound = 2
 	
@@ -528,15 +527,15 @@ end
 -- ======================
 --  HIT MARKER SOUNDS
 -- ======================
-if CheckSetting("cfg_HitMarkerSound", 0) == 1 then
+if CheckSetting("cfg_HitMarkerSound") == 0 then
 	print("ME5_ConfigCheck: Hit Marker Sounds are DISABLED")
 	ME5_HitMarkerSound = 0
 	
-elseif CheckSetting("cfg_HitMarkerSound", 1) == 1 then
+elseif CheckSetting("cfg_HitMarkerSound") == 1 then
 	print("ME5_ConfigCheck: Hit Marker Sounds are QUIET")
 	ME5_HitMarkerSound = 1
 	
-elseif CheckSetting("cfg_HitMarkerSound", 2) == 1 then
+elseif CheckSetting("cfg_HitMarkerSound") == 2 then
 	print("ME5_ConfigCheck: Hit Marker Sounds are LOUD")
 	ME5_HitMarkerSound = 2
 	
